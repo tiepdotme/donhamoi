713b4a23c7519822179d03e1af688a5525bba9b6
Tiep <50001808+tieppu@users.noreply.github.com>
2019-07-16 06:38:44 +0700

    location

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { prefixLink } from 'gatsby-helpers'
-config = require('config');
+config = require('/config');
 import Ink from 'react-ink'
 import HeaderLogo from '../HeaderLogo'
 import MainMenu from '../MainMenu'
